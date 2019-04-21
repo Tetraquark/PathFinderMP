0712b6c7b4b4533eca20e42d909e44816b2d4711
Tetraquark <avladislavv@gmail.com>
2019-04-21 23:37:13 +0700

    [iosApp] button click listeners by targets; possible implementation to change cells colors

--- iosApp/PathFinderMPiOS/PathFinderMPiOS/ViewController.swift
+++ iosApp/PathFinderMPiOS/PathFinderMPiOS/ViewController.swift
@@ -21,9 +21,8 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     /* Default cells count on collectionView */
     var cellsCount = 1
     /* Available algorithms */
-    var pickOption = ["Wave"]
+    var pickOption = ["Wave", "Dijkstra"]
     
-    /* Presenter */
     var presenter = MainPresenter()
 
     override func viewDidLoad() {
@@ -109,27 +108,33 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     }
     
     /* Handle button press */
+    /*
     @IBAction func generateButton(_ sender: UIButton) {
         cellsCount = (Int(cellsInWidth.text!)! * Int(cellsInHeight.text!)!)
-        self.collectionView.reloadData()
+        //self.collectionView.reloadData()
     }
+    */
     
     /* Logic */
     
     func getInputMapWidth() -> Int32 {
-        return Int32(cellsInWidth.text!)!
+        return Int32(cellsInWidth.text!) ?? 0
     }
     
     func getInputMapHeight() -> Int32 {
-        return Int32(cellsInHeight.text!)!
+        return Int32(cellsInHeight.text!) ?? 0
     }
     
     func getSelectedAlgorithm() -> RoutingAlgorithm {
         var algorithm = RoutingAlgorithm.wave
         for item in pickOption {
+            // TODO: missed Dijkstra algorithm
             switch (item) {
             case "Wave":
                 algorithm = RoutingAlgorithm.wave
+                break
+            case "Dijkstra":
+                algorithm = RoutingAlgorithm.dijkstra
                 break
             default:
                 algorithm = RoutingAlgorithm.wave
@@ -158,28 +163,49 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         /* TODO: Nothing to implement in iOS project?
          * This data has been stored in pickOption
          */
+
+         // тут надо динамически заполнять pickOption и обновлять пикер 
     }
     
     func enableGenerateAction() {
         actionButton.isEnabled = true
         actionButton.setTitle("Generate", for: .normal)
+        actionButton.addTarget(self, action: "onGenerateButtonAction:", forControlEvents: UIControlEvents.TouchUpInside)
     }
-    
+
     func disableGenerateAction() {
         actionButton.isEnabled = false
+        actionButton.removeTarget(nil, action: nil, for: .allEvents)
+    }
+
+    func onGenerateButtonAction(sender: UIButton!) {
+        presenter.onGenerateAction()
     }
     
     func enableClearAction() {
         actionButton.isEnabled = true
         actionButton.setTitle("Clear", for: .normal)
+        actionButton.addTarget(self, action: "onClearButtonAction:", forControlEvents: UIControlEvents.TouchUpInside)
     }
     
     func disableClearAction() {
         actionButton.isEnabled = false
+        actionButton.removeTarget(nil, action: nil, for: .allEvents)
+    }
+
+    func onClearButtonAction(sender: UIButton!) {
+        presenter.onClearAction()
     }
     
     func setStartCell(point: KotlinPair) {
-        
+        // тут надо поменять цвет для ячейки, которая находится по индексам point
+        // правильно ли использованы координаты или поменять местами first и second?
+        let indexPath = IndexPath(item: point.second, section: point.first)
+        if let cell = self.collectionView.cellForItem(at: indexPath) {
+            let color = UIColor.green
+            // поменять цвет ячейки collectionView
+            cell.backgroundColor = color
+        } 
     }
     
     func showProgress() {
@@ -199,11 +225,48 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     }
     
     func drawMap(map: WorldMap) {
-
+        // 1) применить к collectionView новые размеры карты (количество ячеек): map.width и map.height
+        // 2) отрисовать карту (или она сама отрисуется?)
+        // обход карты для покраски ячеек можно сделать через итератор:
+        let it = map.iterator()
+        while it.hasNext() {
+            let mapCell = it.next()
+            // 1) получить цвет ячейки исходя из типа mapCell.cellType (enum с константами: OPEN и BLOCK)
+            // Если OPEN - то цвет белый/светлый, если BLOCK - то цвет темный/серый
+            let color = UIColor.white
+            // 2) применить цвет к ячейке по координатам mapCell.x и mapCell.y
+            // правильно ли использованы координаты или поменять местами x и y?
+            let indexPath = IndexPath(item: mapCell.y, section: mapCell.x)
+            if let cell = self.collectionView.cellForItem(at: indexPath) {
+                cell.backgroundColor = color
+            }
+        }
     }
     
     func drawPath(path: Path) {
+        let pathLen = path.count()
         
+        // обход пути с помощью for
+        // если не сработает, то можно через итератор: path.iterator()
+        /*
+        let it = path.iterator()
+        while it.hasNext() {
+            let mapCell = it.next()
+        }
+        */
+        for mapCell in path {
+            // правильно ли использованы координаты или поменять местами x и y?
+            let indexPath = IndexPath(item: mapCell.y, section: mapCell.x)
+            if let cell = self.collectionView.cellForItem(at: indexPath) {
+                // 1) если ячейка первая (индекс == 0), то получить цвет стартовой ячейки
+                // 2) если ячейка последняя (индекс == pathLen - 1), то получить цвет финальной ячейки
+                // 3) иначе получить дефолтный цвет ячейки пути
+                let color = UIColor.white
+                
+                // поменять цвет ячейки collectionView
+                cell.backgroundColor = color
+            } 
+        }
     }
     
     func clearMap() {
@@ -213,6 +276,10 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     func showError(text: String) {
         /* TODO: Implement later */
     }
+
+    fileprivate func fromCoordsToIndex(x: Int, y: Int, width: Int) -> Int {
+        return x + width * y
+    } 
 }
 
 extension ViewController: UICollectionViewDelegateFlowLayout {
